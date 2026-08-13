c2e6e1db59a183c9c1dea98b22d3d2e6f22a613e
rakusuki <chuusuke@gmail.com>
2026-08-13 19:21:19 +0900

    add bookmarklet code.

---
+++ bmlet.js
@@ -0,0 +1,6 @@
+javascript:(()=>{
+    const s = document.createElement("script");
+    s.src = "https://cdn.jsdelivr.net/gh/rakusuki/before_n_business_days@main/before_N_business_days.js";
+    document.documentElement.appendChild(s);
+    s.remove();
+})();
